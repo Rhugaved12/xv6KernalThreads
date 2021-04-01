b6525c753b47da445151c04fc88282dd885bf16b
rhugaved <narmaderr18.comp@coep.ac.in>
2021-04-01 16:12:14 +0530

    Changed process structure

--- proc.h
+++ proc.h
@@ -49,6 +49,9 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+   // Rhugaved Edit begins
+  void *thread_stack ;         // Pointer to a stack for thread
+  int is_thread;               // To distinguish between a process and a thread
 };
 
 // Process memory is laid out contiguously, low addresses first:
